b60d2675e5fb62f9d8d887dd4d12ad4f2bb4bbd5
hsgytjr <121750445+hsgytjr@users.noreply.github.com>
2023-03-27 12:33:33 +0900

    player

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public float MoveSpeed
+    {
+        set => anim.SetFloat("movementSpeed", value);
+        get => anim.GetFloat("movementSpeed");
+    }
+
+    public void OnReload()
+    {
+        anim.SetTrigger("onReload");
+    }
+
+    public void Play(string _stateName, int _layer, float _normalizedTime)
+    {
+        anim.Play(_stateName, _layer, _normalizedTime);
+    }
+
+    public bool CurrentAnimationIs(string name)
+    {
+        return anim.GetCurrentAnimatorStateInfo(0).IsName(name); // 현재 애니메이션 중인지 확인하고 애니메이션 중이라면 이름을 반환
+    }
+}
