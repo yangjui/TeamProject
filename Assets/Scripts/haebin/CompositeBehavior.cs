954d6a092634f2def02a3781eafe9cec1602f606
LEEhaebhin <123037472+LEEhaebhin@users.noreply.github.com>
2023-03-27 18:40:47 +0900

    test c

---
+++ Assets/Scripts/haebin/CompositeBehavior.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Flock/Behavior/Composite")]
+public class CompositeBehavior : FlockBehaviour
+{
+    public FlockBehaviour[] behaviours;
+    public float[] weights;
+
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+       if(weights.Length != behaviours.Length)
+       {
+            Debug.LogError("Data Missmatch in " + name, this);
+            return Vector3.zero;
+       }
+
+        Vector3 move = Vector3.zero;
+
+        for(int i = 0; i <behaviours.Length; ++i)
+        {
+            Vector3 partialMove = behaviours[i].CalculateMove(agent, context, flock) * weights[i]; 
+
+            if(partialMove != Vector3.zero)
+            {
+                if(partialMove.sqrMagnitude > weights[i] * weights[i])
+                {
+                    partialMove.Normalize();
+                    partialMove *= weights[i];
+                }
+                move += partialMove;
+            }
+        }
+        return move;
+    }
+
+}
