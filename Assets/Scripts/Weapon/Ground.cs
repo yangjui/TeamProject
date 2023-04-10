9abc5e0b805f664f68eb12210919ec2c5022e53b
hsgytjr <121750445+hsgytjr@users.noreply.github.com>
2023-04-10 18:07:18 +0900

    명하형꺼 머지

--- Assets/Scripts/Weapon/Ground.cs
+++ Assets/Scripts/Weapon/Ground.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
+    private void Start()
+    {
+        Destroy(gameObject, 4f);
+    }
 
     private void OnTriggerEnter(Collider _other)
     {
         if (_other.CompareTag("Zombie"))
+        {
+            _other.GetComponent<Target>().Onfire();
+        }
+
+        if (_other.CompareTag("Target"))
         {
             _other.GetComponent<Target>().Onfire();
         }
